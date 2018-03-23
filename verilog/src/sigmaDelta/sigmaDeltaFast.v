6b4e03e2ba4294593cbfb8fbe5a0c71715f3fc0f
Scott Teal <scott.teal@kollmorgen.com>
2018-03-23 16:40:43 -0400

    Adding more sigma-delta modulators

---
+++ verilog/src/sigmaDelta/sigmaDeltaFast.v
@@ -0,0 +1,56 @@
+module sigmaDeltaFast #(
+    parameter WIDTH = 4,
+    parameter OUTLEN = (1 << WIDTH)
+)
+(
+    input clk,                    ///< System clock
+    input rst,                    ///< Reset, synchronous active high
+    input en,                     ///< Enable to run the modulator
+    input signed [WIDTH-1:0] in,  ///< Input to modulator
+    output reg [OUTLEN-1:0] sdOut ///< Sigma delta stream, LSB=first sample, MSB=last sample
+);
+
+localparam DEPTH = 2**(2*WIDTH);
+
+reg signed [WIDTH-1:0] feedback;
+
+reg [WIDTH-1:0] fbLookup [DEPTH-1:0];
+reg [OUTLEN-1:0] outLookup [DEPTH-1:0];
+
+integer i;
+integer j;
+integer k;
+
+reg [WIDTH-1:0] tempFb;
+reg [WIDTH-1:0] tempIn;
+
+initial begin
+    feedback = 'd0;
+    sdOut = 32'hAAAA_AAAA;
+    // i = input
+    // j = feedback
+    // k = bit in sequence
+    for (i=0; i<(2**WIDTH); i=i+1) begin
+        for (j=0; j<(2**WIDTH); j=j+1) begin
+            tempIn = i[WIDTH-1:0];
+            tempFb = j[WIDTH-1:0];
+            for (k=0; k<OUTLEN; k=k+1) begin
+                {outLookup[{i[WIDTH-1:0],j[WIDTH-1:0]}][k], tempFb} = tempFb + {~tempIn[WIDTH-1], tempIn[WIDTH-2:0]};
+            end
+            fbLookup[{i[WIDTH-1:0],j[WIDTH-1:0]}] = tempFb;
+        end
+    end
+end
+
+always @(posedge clk) begin
+    if (rst) begin
+        feedback <= 'd0;
+        sdOut <= 32'hAAAA_AAAA;
+    end
+    else if (en) begin
+        sdOut <= outLookup[{in, feedback}];
+        feedback <= fbLookup[{in,feedback}];
+    end
+end
+
+endmodule
---
+++ verilog/src/sigmaDelta/test/sigmaDelta1stOrder_tb.v
@@ -0,0 +1,73 @@
+module sigmaDelta1stOrder_tb ();
+
+parameter OUT_WIDTH = 8;
+parameter WIDTH = 16;
+
+// UUT Signals
+reg clk;                    ///< System Clock
+reg rst;                    ///< Reset, active high & synchronous
+reg en;                     ///< Enable (use to clock at slower rate)
+reg signed [WIDTH-1:0] in;
+wire [OUT_WIDTH-1:0] sdOut; ///< Sigma-delta input
+wire sdOut2;
+
+// Testbench Signals
+wire [WIDTH-1:0] out; ///< Magnitude of in
+integer i;
+
+always #1 clk = ~clk;
+
+initial begin
+    clk    = 1'b0;
+    rst    = 1'b1;
+    en     = 1'b1;
+    in = 'd0;
+    @(posedge clk) rst = 1'b1;
+    @(posedge clk) rst = 1'b1;
+    @(posedge clk) rst = 1'b0;
+    #10000 in =  (2**(WIDTH-1))-1;
+    #10000 in = -(2**(WIDTH-1));
+    #10000 in =  (2**(WIDTH-2));
+    #10000 in = -(2**(WIDTH-2));
+    #10000 in = 0;
+    for (i=0; i<2**18; i=i+1) begin
+        @(posedge clk) in = $rtoi((2.0**(WIDTH-1)-1)*$sin(3.141259*2.0*($itor(i)/2.0**15 + $itor(i)**2/2.0**22)));
+    end
+    #10000 $stop();
+end
+
+sigmaDelta1stOrder #(
+    .WIDTH(WIDTH),
+    .OUT_WIDTH(OUT_WIDTH)
+) 
+uut (
+    .clk(clk),
+    .rst(rst),
+    .en(en),
+    .in(in), ///< [WIDTH-1:0] 
+    .sdOut(sdOut)
+);
+
+sigmaDelta1stOrder #(
+    .WIDTH(WIDTH),
+    .OUT_WIDTH(1)
+) 
+uut2 (
+    .clk(clk),
+    .rst(rst),
+    .en(en),
+    .in(in), ///< [WIDTH-1:0] 
+    .sdOut(sdOut2)
+);
+
+sinc3Filter #(
+    .OSR(32) // Output width is 3*ceil(log2(OSR))+1
+)
+filterOut (
+    .clk(clk),
+    .en(en), ///< Enable (use to clock at slower rate)
+    .in(sdOut2),
+    .out(out) ///< [3*$clog2(OSR):0] 
+);
+
+endmodule
